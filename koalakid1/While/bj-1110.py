026820bba3584913bbd0b348228050c69fc8a7f3
koalakid1 <koalakid154@gmail.com>
2021-01-30 18:12:22 +0900

    koalakid-While

---
+++ koalakid1/While/bj-1110.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+num = input().strip()
+result = [int(num)]
+
+while True:
+    add = 0
+    for i in str(result[-1]):
+        add += int(i)
+    num = int(str(add)[-1]) + int(str(result[-1])[-1]) * 10
+    print(num)
+    if num == result[0]:
+        break
+    result.append(num)
+
+print(len(result))
