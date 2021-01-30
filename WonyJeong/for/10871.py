cdc065512689605df576259462c1310cabd677b9
WonyJeong <59948675+WonyJeong@users.noreply.github.com>
2021-01-30 16:52:27 +0900

    [WonyJeong] for단계문제 풀이
    [WonyJeong] for단계문제 풀이

--- WonyJeong/for/10871.py
+++ WonyJeong/for/10871.py
@@ -1,11 +1,14 @@
 import sys
+
 input = sys.stdin.readline
 
-def listFilter(x,y):
+
+def listFilter(x, y):
     return x < y
 
-N ,M = map(int, input().strip().split())
+
+N, M = map(int, input().strip().split())
 arr = list(map(int, input().strip().split()))
-result = [x for x in arr if listFilter(x,M)]
+result = [x for x in arr if listFilter(x, M)]
 for x in result:
     print(x, end=" ")
