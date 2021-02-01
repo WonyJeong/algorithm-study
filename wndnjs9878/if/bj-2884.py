3f7c0c2d4ba01a129ccc5c2d1e292a58933bb499
wndnjs9878 <48276633+wndnjs9878@users.noreply.github.com>
2021-02-01 16:47:39 +0900

    [leeez] bj-2739.py

--- wndnjs9878/if/bj-2884.py
+++ wndnjs9878/if/bj-2884.py
@@ -1,4 +1,13 @@
 #2884
+'''
+import sys
+input = sys.stdin.readline
+hour, minute = map(int,input().strip().split())
+if minute - 45 >= 0 : print(hour, minute-45)
+elif hour-1 >= 0 : print(hour-1, 60 + (minute - 45))
+else : print(23, 60 + (minute-45))
+'''
+
 import sys
 input = sys.stdin.readline
 time = list(map(int,input().strip().split()))
