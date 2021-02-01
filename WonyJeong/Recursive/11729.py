fe24fec84ba0ef857e08308e5c2e906cf07a3356
WonyJeong <59948675+WonyJeong@users.noreply.github.com>
2021-02-01 10:24:02 +0900

    [Wony]재귀 단계문제 풀이
    [Wony]재귀 단계문제 풀이

---
+++ WonyJeong/Recursive/11729.py
@@ -0,0 +1,19 @@
+# import sys
+
+# input = sys.stdin.readline
+
+
+# def hanoi(a, b, c, N, history):
+#     if c == [i for i in range(1, N + 1)]:
+#         return
+
+#     # if
+#     print(top)
+
+
+# if __name__ == "__main__":
+#     N = int(input().strip())
+#     a = [i for i in range(1, N + 1)]
+#     b = []
+#     c = []
+#     hanoi(a, b, c, N, [])
