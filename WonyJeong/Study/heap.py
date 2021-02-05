3a2bc10938178eed1d500bc3fa35916f477c58f6
WonyJeong <59948675+WonyJeong@users.noreply.github.com>
2021-02-05 12:25:56 +0900

    [Wony] 최단거리 문제풀이
    [Wony] 최단거리 문제풀이

---
+++ WonyJeong/Study/heap.py
@@ -0,0 +1,12 @@
+import heapq
+from queue import PriorityQueue
+
+heap = []
+
+heapq.heappush(heap, 4)
+heapq.heappush(heap, 1)
+heapq.heappush(heap, 7)
+heapq.heappush(heap, 3)
+
+
+que = PriorityQueue()
