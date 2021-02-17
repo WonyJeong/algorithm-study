e0fbf0d44108b435277b3c197060270252c58ed6
wndnjs9878 <48276633+wndnjs9878@users.noreply.github.com>
2021-02-18 03:00:46 +0900

    [leeez] Vanilla study

--- wndnjs9878/FrontEnd/Vanilla/Momentum/clock.js
+++ wndnjs9878/FrontEnd/Vanilla/Momentum/clock.js
@@ -2,16 +2,16 @@
 // 윈도우 사이즈가 변경될 때만!!!!! handleResize 함수 호출하자!!!
 // handleResize()라고 적게 되면 지금 바로 함수를 호출하라는 의미
 
+//classList.add(A) : className에 A 덧붙여서 추가
+//classList.remove(A) : className에 A 덧붙여진거 삭제
+//classList.toggle(A) : className에 A 없으면 추가 있으면 삭제
+//toggle do that if there is a class, remove the class from classList. Likewise, If there isnt a class, add the class to classList
+
 const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked"
 
 function handlieClicked() {
-    const currentClassName = title.className;
-    if (currentClassName !== CLICKED_CLASS) {
-        title.className = CLICKED_CLASS;
-    } else {
-        title.className = "";
-    }
+    title.classList.toggle(CLICKED_CLASS)
 }
 
 function init() {
