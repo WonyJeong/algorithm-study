54f499a365e21fc491e5b7eb637bd51d28dcaf7f
WonyJeong <59948675+WonyJeong@users.noreply.github.com>
2021-02-17 23:26:55 +0900

    [wony] 구현
    [wony] 구현

--- WonyJeong/implementation/easy.py
+++ WonyJeong/implementation/easy.py
@@ -1,7 +1,50 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
+dx = [0, 0, 0, -1, 1]
+dy = [0, 1, -1, 0, 0]
+
+
+def moveDice(side, front, k):
+    temp1 = front
+    temp2 = side
+
+    temp1[0] = 100
+
+    print(temp)
+    print(front)
+    # if k == 1:
+
+    return 0
+
+
+def dice():
+
+    for k in dir_:
+        side[3] = map_[x][y]
+        front[3] = map_[x][y]
+
+        side, front = moveDice(side, front, k)
+
+        nx = x + dx[k]
+        ny = y + dy[k]
+
+        if 0 <= nx < N and 0 <= ny < M:
+            x = nx
+            y = ny
+
 
 if __name__ == "__main__":
-    N = int(input().strip())
+    global N, M, x, y, K, side, front, map_, dir_
+    N, M, x, y, K = map(int, input().strip().split())
+    side = deque([0, 0, 0, 0])
+    front = deque([0, 0, 0, 0])
+    map_ = []
+    for _ in range(N):
+        map_.append(list(map(int, input().strip().split())))
+
+    dir_ = list(map(int, input().strip().split()))
+
+    dice()
