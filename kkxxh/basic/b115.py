7f091c28e118cb23dffdd7b4f7c95048e7b1afb4
kangseonghee <59370701+kkxxh@users.noreply.github.com>
2021-02-23 09:08:05 +0900

    kkxxh _ basic-search

---
+++ kkxxh/basic/b115.py
@@ -0,0 +1,25 @@
+#seq_search()함수를 사용하여
+# 실수 검색
+
+from b114 import seq_search
+
+print('실수 검색')
+print('주의 : "End"를 입력할 경우 종료합니다.')
+
+number = 0
+x = []
+
+while True :
+    s = input(f'x[{number}]: ')
+    if s == 'End':
+        break
+    x.append(float(s))
+    number += 1
+
+ky = float(input('검색할 값 입력 : '))
+
+idx = seq_search(x, ky)
+if idx == -1:
+    print('검색값을 갖는 원소가 존재하지 않습니다.')
+else : 
+    print(f'검색값은 x[{idx}]에 있습니다.')
