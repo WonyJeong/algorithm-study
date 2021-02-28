29e8d768432f3735100fafa609240e01b85c2e70
wndnjs9878 <48276633+wndnjs9878@users.noreply.github.com>
2021-02-28 23:22:41 +0900

    [leeez] union find

--- wndnjs9878/UnionFind/bj-1717.py
+++ wndnjs9878/UnionFind/bj-1717.py
@@ -1,36 +1,44 @@
 #1717
+'''
+1. parent를 자기 자신으로 초기화
+2. 부모를 찾는 find함수
+3. 합집합 연산 -> 같은 부모를 갖도록 union함수 제작
+4. 연산을 입력받아 0이면 합집합 연산
+                1이면 find를 통해 부모를 찾고 부모가 같으면 같은집합, 다르면 다른 집합에 포함되어있다
+'''
+
 import sys
 input = sys.stdin.readline
-mm={}
-def find_parent(x):
-    if mm[x]==x:
-        return x
-    else:
-        return find_parent(mm[x])
-def get_parent(x):
-    if mm[x]==x:
-        return x
-    else:
-        mm[x]=get_parent(mm[x])
-        return mm[x]
-def union(x,y):
-    x=get_parent(x)
-    y=get_parent(y)    
-    if x!=y:
-        mm[y]=x
-        
-        
-n,m = map(int, input().split())
-for i in range(n+1):
-    mm[i]=i
-for _ in range(m):
-    try:
-        v,a,b=map(int, input().split())
-        if not v:
+
+def find(target):
+    if target == parent[target]:
+        return target
+    
+    parent[target] = find(parent[target])
+    return parent[target]
+
+def union(a,b):
+    a = find(a)
+    b = find(b)
+
+    if a < b:
+        parent[b] = a
+    else :
+        parent[a] = b
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().strip().split())
+    parent = [i for i in range(n+1)]
+
+    for _ in range(m):
+        flag, a, b = map(int, input().strip().split())
+        if flag :
+            if find(a) == find(b):
+                print('YES')
+            else :
+                print('NO')
+        else :
             union(a,b)
-        elif find_parent(a)==find_parent(b):
-            print("YES") 
-        else:
-            print("NO")
-    except:
-        pass
+
+
