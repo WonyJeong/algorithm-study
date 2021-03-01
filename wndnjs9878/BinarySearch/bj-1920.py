22d3326da67bb4603fbfd7b8fb8eb29d6dc70377
wndnjs9878 <48276633+wndnjs9878@users.noreply.github.com>
2021-03-01 23:27:52 +0900

    [leeez] binarysearch 1920

---
+++ wndnjs9878/BinarySearch/bj-1920.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+def solution(N, A, M, check):
+    A = sorted(A)
+    for ch in check :
+        start = 0
+        end = N-1
+        target = ch
+        answer = 0
+        while (start <= end):
+            mid = (start + end) // 2
+            if A[mid] == target :
+                answer = 1
+                break
+            elif target < A[mid] :
+                end = mid - 1
+            else : 
+                start = mid +1
+        print(answer)
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    A = list(map(int, input().strip().split()))
+    M = int(input().strip())
+    check = list(map(int, input().strip().split()))
+    solution(N, A, M, check)
+
