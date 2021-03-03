4c14185ec3f4d65e62e7a378104806e316db7efa
wndnjs9878 <48276633+wndnjs9878@users.noreply.github.com>
2021-03-03 19:12:48 +0900

    [leeez] 1300 ing...

--- wndnjs9878/BinarySearch/bj-1300.py
+++ wndnjs9878/BinarySearch/bj-1300.py
@@ -3,8 +3,20 @@ import sys
 input = sys.stdin.readline
 
 def solved(N,K):
+    start = 1
+    end = pow(N,2)
+    step = 0
 
-
+    while start <= end :
+        mid = (start + end) // 2 
+        step  += 1
+        if K == mid :
+            print(step * (step+1))
+            break
+        elif mid < K :
+            start = mid + 1
+        else :
+            end = mid - 1
 
 if __name__ == '__main__':
     N = int(input().strip())
