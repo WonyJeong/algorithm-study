0988b81eee46e4accff51e5f5529195af49d4a6f
wndnjs9878 <48276633+wndnjs9878@users.noreply.github.com>
2021-03-08 12:35:53 +0900

    [leeez] dp 2579

--- wndnjs9878/DP/bj-2579.py
+++ wndnjs9878/DP/bj-2579.py
@@ -6,15 +6,15 @@ def solution(N,stairs):
     if N >= 3:
         answer = []
         answer.append(stairs[0])
-        answer.append(answer[0]+stairs[1])
-        answer.append(max(answer[0]+stairs[2], stairs[1]+stairs[2]))
+        answer.append(stairs[0]+stairs[1])
+        answer.append(max(stairs[0]+stairs[2], stairs[1]+stairs[2]))
 
         for i in range(3,N):
             answer.append(max(stairs[i] + stairs[i-1] + answer[i-3], stairs[i] + answer[i-2]))
         
         return answer[-1]
     elif N == 2:
-        return max(stairs[0]+stairs[1], stairs[1])
+        return stairs[0]+stairs[1]
     elif N == 1 :
         return stairs[0]
 
